ce96132a12d1baaf6104e2fe7df27dd7d1172efa
Abdul Rahman Almowakea <ABA022@diyarme.com>
2023-08-09 23:14:42 +0300

    Signalr Service Implementation

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { SignalRService } from '../services/signalr.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+
+export class HomePageComponent implements OnInit {
     title = 'Home Page';
+
+    constructor(private signalRService: SignalRService) { }
+
+    ngOnInit(): void {
+        this.signalRService.hubConnection.on('MessageReceived', (msg: string) => {
+            console.info('message received from server');
+            console.info(msg);
+        });
+    }
 }
