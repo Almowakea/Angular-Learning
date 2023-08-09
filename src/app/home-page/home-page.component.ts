d035cfcf53fdb1b39b16694cbaec2c36e33afbd3
Abdul Rahman Almowakea <ABA022@diyarme.com>
2023-08-09 23:20:53 +0300

    test2

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -10,7 +10,6 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
     title = 'Home Page';
 
-
     constructor(private signalRService: SignalRService) { }
 
     ngOnInit(): void {
